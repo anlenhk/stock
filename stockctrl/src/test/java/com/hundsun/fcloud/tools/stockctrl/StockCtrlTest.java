7e2912bb277f5dc967276bde3863323b7f6d4d55
Gavin Hu <haozhonghu@hotmail.com>
2015-02-12 16:12:24 +0800

    单元测试用例

--- stockctrl/src/test/java/com/hundsun/fcloud/tools/stockctrl/StockCtrlTest.java
+++ stockctrl/src/test/java/com/hundsun/fcloud/tools/stockctrl/StockCtrlTest.java
@@ -8,6 +8,13 @@ import com.hundsun.fcloud.servlet.caller.pool.PoolableServletCaller;
 import com.hundsun.fcloud.servlet.share.DefaultServletRequest;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 /**
  * Created by Gavin Hu on 2015/1/19.
  */
@@ -16,31 +23,54 @@ public class StockCtrlTest {
     @Test
     public void testLock() {
         //
-        ServletCaller servletCaller = new PoolableServletCaller(new String[]{"localhost"}, new int[]{6161}, 5);
+        final ServletCaller servletCaller = new PoolableServletCaller(new String[]{"localhost", "192.168.190.191"}, new int[]{6161, 6161}, 10);
         //
-        ServletRequest servletRequest = new DefaultServletRequest();
-        servletRequest.setHeader(ServletMessage.HEADER_CODEC, "26");
-        servletRequest.setParameter("funcNo", "26");
-        servletRequest.setParameter("entrust", "1");
-        servletRequest.setParameter("netNo", "8888");
-        servletRequest.setParameter("operatorCode", "06843");
+        List<Future> futureList = new ArrayList<Future>();
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for(int i=0; i<200; i++) {
+            //
+            final int finalI = i;
+            Future future = executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    //
+                    try {
+                        ServletRequest servletRequest = new DefaultServletRequest();
+                        servletRequest.setHeader(ServletMessage.HEADER_CODEC, "26");
+                        servletRequest.setParameter("funcNo", "26");
+                        servletRequest.setParameter("entrust", "1");
+                        servletRequest.setParameter("netNo", "8888");
+                        servletRequest.setParameter("operatorCode", "06843");
+                        //
+                        servletRequest.setParameter("requestNo", "1000" + String.valueOf(finalI));
+                        servletRequest.setParameter("balance", "4000");
+                        servletRequest.setParameter("operateCode", "0");
+                        servletRequest.setParameter("tradeAcco", "T200000000000" + String.valueOf(finalI));
+                        servletRequest.setParameter("fundCode", "600570");
+                        servletRequest.setParameter("bizCode", "022");
+                        //
+                        ServletResponse servletResponse = servletCaller.call(servletRequest);
+                        System.out.println(servletResponse.getParameter("flag"));
+                        System.out.println(servletResponse.getParameter("errorCode"));
+                        System.out.println(servletResponse.getParameter("errorMsg"));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            //
+            futureList.add(future);
+        }
         //
-        servletRequest.setParameter("requestNo", "20000003");
-        servletRequest.setParameter("balance", "4000");
-        servletRequest.setParameter("operateCode", "1");
-        servletRequest.setParameter("tradeAcco", "T2000000000000001");
-        servletRequest.setParameter("fundCode", "600570");
-        servletRequest.setParameter("bizCode", "021");
-        //
-        ServletResponse servletResponse = servletCaller.call(servletRequest);
-        System.out.println(servletResponse.getParameter("flag"));
-        System.out.println(servletResponse.getParameter("errorCode"));
-        System.out.println(servletResponse.getParameter("errorMsg"));
-        //System.out.println(servletResponse.getParameter("totalBalance"));
-        //System.out.println(servletResponse.getParameter("remainBalance"));
-
-        servletRequest.setParameter("operateCode", "3");
-
+        for(Future future : futureList) {
+            try {
+                future.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
         //
         servletCaller.close();
     }
